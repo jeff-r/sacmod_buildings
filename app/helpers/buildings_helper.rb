83d59b56bfc695f445a81c6139e0c9daeb730a25
Jeff Roush <jeff@jeffroush.com>
2017-07-29 21:11:09 -0700

    Linkify gsv

--- app/helpers/buildings_helper.rb
+++ app/helpers/buildings_helper.rb
@@ -1,2 +1,8 @@
 module BuildingsHelper
+  def gsv_or_link(gsv)
+    return "" if gsv.nil?
+    return "" if gsv.empty?
+    return gsv unless /^http/ =~ gsv
+    "<a href=#{gsv}>#{gsv}</a>".html_safe
+  end
 end
