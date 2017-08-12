85f066ce5d9a6c421c96b18dd4a8cbd1bed855e7
Jeff Roush <jeff@jeffroush.com>
2017-08-12 12:26:27 -0700

    Add previous and next links for architects

--- app/helpers/architects_helper.rb
+++ app/helpers/architects_helper.rb
@@ -1,2 +1,9 @@
 module ArchitectsHelper
+  def next_architect_link(architect)
+    link_to "next", architect.next
+  end
+
+  def prev_architect_link(architect)
+    link_to "previous", architect.prev
+  end
 end
--- app/models/architect.rb
+++ app/models/architect.rb
@@ -1,3 +1,11 @@
 class Architect < ApplicationRecord
   has_many :buildings
+
+  def prev
+    Architect.where("id < ?", id).order("id desc").first || Architect.last
+  end
+
+  def next
+    Architect.where("id > ?", id).first || Architect.first
+  end
 end
