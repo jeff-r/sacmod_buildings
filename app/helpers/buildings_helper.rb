4c26b262cc71a67e6f704eae2bb52b1731df9432
Jeff Roush <jeff@jeffroush.com>
2017-08-12 11:38:23 -0700

    Remove google street view link from show page

--- app/helpers/buildings_helper.rb
+++ app/helpers/buildings_helper.rb
@@ -1,11 +1,4 @@
 module BuildingsHelper
-  def gsv_or_link(gsv)
-    return "" if gsv.nil?
-    return "" if gsv.empty?
-    return gsv unless /^http/ =~ gsv
-    "<a href=#{gsv}>#{gsv}</a>".html_safe
-  end
-
   def google_static_map(center)
     image_tag "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=300x300&zoom=17&markers=#{center}"
   end
