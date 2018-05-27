420ffa32494b74fd26e21fcf21c55be3df7d5e18
Jeff Roush <jeff@jeffroush.com>
2018-05-27 12:00:03 -0700

    Bump the max uploaded image size to 10MB

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,10 +20,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
-
+  def size_range
+    0..10.megabytes
+  end
 end
